ec9294bc926fbed680b83e31fc4bbf15ed2be704
Prashant <prashant.patel77@gmail.com>
2020-08-08 18:42:37 +1000

    Fixed bloc usage

--- lib/bloc/weather_bloc.dart
+++ lib/bloc/weather_bloc.dart
@@ -1,12 +1,11 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc_cubit_weather/data/model/weather.dart';
 import 'package:flutter_bloc_cubit_weather/data/weather_repository.dart';
 
 part 'weather_event.dart';
-
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
@@ -18,6 +17,14 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   Stream<WeatherState> mapEventToState(
     WeatherEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is GetWeather) {
+      try {
+        yield WeatherLoading();
+        final weather = await _weatherRepository.fetchWeather(event.cityName);
+        yield WeatherLoaded(weather);
+      } on NetworkException {
+        yield WeatherError("Couldn't fetch weather. Is the device online?");
+      }
+    }
   }
 }
